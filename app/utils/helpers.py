7413bdef92ea4cce438e8ebbb8f5fbecfcdbae1a
Lucas Machado <54224302+lfjmachado@users.noreply.github.com>
2025-05-11 20:13:06 -0300

    add funcao helper para validar numero inteiro

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,3 +1,10 @@
 def clean_quantity(value):
     value = value.replace('.', '').replace('-', '0')
     return int(value) if value.isdigit() else 0
+
+def valid_is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
